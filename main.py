7ed0d0b49e039b7325b6deef8c0cbb568fa84086
Durga <73925384+Durgareddyp@users.noreply.github.com>
2023-11-05 17:59:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+
+html_code = """
+<div className="flex flex-col min-h-screen bg-gray-200 p-4 md:p-0">
+  <main className="flex flex-col items-center justify-center w-full">
+    <section className="max-w-3xl w-full mt-16 grid grid-cols-1 md:grid-cols-1 gap-4">
+    </section>
+  </main>
+</div>
+"""
+
+# Parse the HTML code
+soup = BeautifulSoup(html_code, 'html.parser')
+
+# Find all elements with a className attribute
+elements_with_class = soup.find_all(attrs={"class": True})
+
+# Remove the className attribute from each element
+for element in elements_with_class:
+    del element['class']
+
+# Print the modified HTML
+print(soup.prettify())
